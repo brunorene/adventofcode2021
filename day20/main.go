d252c0e370fe8e94bfa4147d382a493a25923a12
bsa11 <bruno.santos@sky.uk>
2021-12-22 17:42:55 +0000

    Day 20 - more functions

--- day20/main.go
+++ day20/main.go
@@ -6,13 +6,23 @@ import (
 	"math"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
 type imageGrid struct {
 	algorithm              []int
 	image                  map[string]int
+	values                 map[string]int
 	minX, maxX, minY, maxY int
+}
+
+func (im imageGrid) cell(x, y int) int {
+	return im.image[fmt.Sprintf("%d%d", x, y)]
+}
+
+func (im imageGrid) value(x, y int) int {
+	return im.values[fmt.Sprintf("%d%d", x, y)]
 }
 
 func processInput(lines []string) (image imageGrid) {
@@ -51,14 +61,30 @@ func processInput(lines []string) (image imageGrid) {
 	return
 }
 
-func (im *imageGrid) applyAlgorithm() {
-	
+func (im *imageGrid) calculateValues() {
+	im.values = make(map[string]int)
+
+	for y := im.minY - 1; y <= im.maxY+1; y++ {
+		for x := im.minX - 1; x <= im.maxX+1; x++ {
+			binary := fmt.Sprintf("%d%d%d%d%d%d%d%d%d",
+				im.cell(x-1, y-1), im.cell(x, y-1), im.cell(x+1, y-1),
+				im.cell(x-1, y), im.cell(x, y), im.cell(x+1, y),
+				im.cell(x-1, y+1), im.cell(x, y+1), im.cell(x+1, y+1),
+			)
+
+			val, err := strconv.ParseInt(binary, 2, 32)
+			check(err)
+
+			im.values[fmt.Sprintf("%d,%d", x, y)] = int(val)
+		}
+	}
 }
 
 func part1() {
 	lines := readInput("input.txt")
 
 	img := processInput(lines)
+	img.calculateValues()
 	fmt.Printf("%+v", img)
 
 	fmt.Printf("%d\n", len(lines))
