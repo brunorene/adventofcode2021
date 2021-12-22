7aad6d000d8c2e54d35df234b2e3bd55d11c7b26
bsa11 <bruno.santos@sky.uk>
2021-12-22 00:35:51 +0000

    Day 21

--- day21/main.go
+++ day21/main.go
@@ -3,26 +3,61 @@ package main
 import "fmt"
 
 const (
-	P1StartPosition = 1
-	P2StartPosition = 9
-	P1              = -1
-	P2              = 1
+	P1StartPosition = 2
+	P2StartPosition = 10
+	P1              = true
+	P2              = false
 )
 
 type gameState struct {
-	score int
+	score    int
 	position int
 }
 
-func play(player int, wins map[int]int64, )
+func quantumPlay(player bool, p1Pos int, p1Score int, p2Pos int, p2Score int, roll int) (p1Wins, p2Wins int64) {
+	switch player {
+	case P1:
+		p1Pos = (p1Pos+roll-1)%10 + 1
+		p1Score += p1Pos
+		if p1Score >= 21 {
+			return 1, 0
+		}
+	case P2:
+		p2Pos = (p2Pos+roll-1)%10 + 1
+		p2Score += p2Pos
+		if p2Score >= 21 {
+			return 0, 1
+		}
+	}
+
+	p1Wins9, p2Wins9 := quantumPlay(!player, p1Pos, p1Score, p2Pos, p2Score, 9)
+	p1Wins8, p2Wins8 := quantumPlay(!player, p1Pos, p1Score, p2Pos, p2Score, 8)
+	p1Wins7, p2Wins7 := quantumPlay(!player, p1Pos, p1Score, p2Pos, p2Score, 7)
+	p1Wins6, p2Wins6 := quantumPlay(!player, p1Pos, p1Score, p2Pos, p2Score, 6)
+	p1Wins5, p2Wins5 := quantumPlay(!player, p1Pos, p1Score, p2Pos, p2Score, 5)
+	p1Wins4, p2Wins4 := quantumPlay(!player, p1Pos, p1Score, p2Pos, p2Score, 4)
+	p1Wins3, p2Wins3 := quantumPlay(!player, p1Pos, p1Score, p2Pos, p2Score, 3)
+	return p1Wins9 + p1Wins8*3 + p1Wins7*6 + p1Wins6*7 + p1Wins5*6 + p1Wins4*3 + p1Wins3, p2Wins9 + p2Wins8*3 + p2Wins7*6 + p2Wins6*7 + p2Wins5*6 + p2Wins4*3 + p2Wins3
+}
+
+func startGame(p1Start, p2Start int) (p1Wins, p2Wins int64) {
+	p1Wins9, p2Wins9 := quantumPlay(P1, p1Start, 0, p2Start, 0, 9)
+	p1Wins8, p2Wins8 := quantumPlay(P1, p1Start, 0, p2Start, 0, 8)
+	p1Wins7, p2Wins7 := quantumPlay(P1, p1Start, 0, p2Start, 0, 7)
+	p1Wins6, p2Wins6 := quantumPlay(P1, p1Start, 0, p2Start, 0, 6)
+	p1Wins5, p2Wins5 := quantumPlay(P1, p1Start, 0, p2Start, 0, 5)
+	p1Wins4, p2Wins4 := quantumPlay(P1, p1Start, 0, p2Start, 0, 4)
+	p1Wins3, p2Wins3 := quantumPlay(P1, p1Start, 0, p2Start, 0, 3)
+	return p1Wins9 + p1Wins8*3 + p1Wins7*6 + p1Wins6*7 + p1Wins5*6 + p1Wins4*3 + p1Wins3, p2Wins9 + p2Wins8*3 + p2Wins7*6 + p2Wins6*7 + p2Wins5*6 + p2Wins4*3 + p2Wins3
+}
 
 func determPlay(p1Pos, p2Pos int) (result int) {
-	scores := map[int]int{P1: 0, P2: 0}
-	positions := map[int]int{P1: p1Pos, P2: p2Pos}
+	scores := map[bool]int{P1: 0, P2: 0}
+	positions := map[bool]int{P1: p1Pos, P2: p2Pos}
 
 	player := P1
 
-	draw := -1
+	draw := 0
 
 	rolls := 0
 
@@ -30,22 +65,22 @@ func determPlay(p1Pos, p2Pos int) (result int) {
 		play := 0
 		for i := 0; i < 3; i++ {
 			draw++
-			draw = draw % 100
-			play += draw + 1
+			draw = (draw-1)%100 + 1
+			play += draw
 			rolls++
 		}
 
-		positions[player] = (positions[player] + play) % 10
-		scores[player] += positions[player] + 1
+		positions[player] = (positions[player]+play-1)%10 + 1
+		scores[player] += positions[player]
 
 		if scores[player] >= 1000 {
 			break
 		}
 
-		player = -player
+		player = !player
 	}
 
-	return scores[-player] * rolls
+	return scores[!player] * rolls
 }
 
 func part1() {
@@ -53,6 +88,8 @@ func part1() {
 }
 
 func part2() {
+	p1Wins, p2Wins := startGame(P1StartPosition, P2StartPosition)
+	fmt.Printf("%d %d\n", p1Wins, p2Wins)
 }
 
 func main() {
--- day21/main_test.go
+++ day21/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_determPlay(t *testing.T) {
 	type args struct {
@@ -14,7 +16,7 @@ func Test_determPlay(t *testing.T) {
 	}{
 		{
 			"test 1",
-			args{3, 7},
+			args{4, 8},
 			739785,
 		},
 	}
@@ -22,6 +24,37 @@ func Test_determPlay(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotResult := determPlay(tt.args.p1Pos, tt.args.p2Pos); gotResult != tt.wantResult {
 				t.Errorf("determPlay() = %v, want %v", gotResult, tt.wantResult)
+			}
+		})
+	}
+}
+
+func Test_startGame(t *testing.T) {
+	type args struct {
+		p1Start int
+		p2Start int
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantP1Wins int64
+		wantP2Wins int64
+	}{
+		{
+			"test 1",
+			args{4, 8},
+			444356092776315,
+			341960390180808,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotP1Wins, gotP2Wins := startGame(tt.args.p1Start, tt.args.p2Start)
+			if gotP1Wins != tt.wantP1Wins {
+				t.Errorf("startGame() gotP1Wins = %v, want %v", gotP1Wins, tt.wantP1Wins)
+			}
+			if gotP2Wins != tt.wantP2Wins {
+				t.Errorf("startGame() gotP2Wins = %v, want %v", gotP2Wins, tt.wantP2Wins)
 			}
 		})
 	}
